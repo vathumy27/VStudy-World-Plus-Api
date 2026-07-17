f0797bf2946f9b6d4038f20c78291b99dbf18e7c
Vathumy Vaitheesvaran <vathumyv@gmail.com>
2026-07-17 10:57:00 +0530

    run.py updated

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(
-        debug=app.config.get("FLASK_DEBUG", False),
+        debug=True ,
         host="0.0.0.0",
         port=5000,
     )
